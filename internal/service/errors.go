89f7a9ab1ac0848a9d857b74cb1783b618d72f4b
Muhammad Shahwal <sawalrever23@gmail.com>
2026-01-30 23:19:21 +0700

    feat: implement product service

--- internal/service/errors.go
+++ internal/service/errors.go
@@ -6,3 +6,9 @@ var (
 	ErrCategoryNotFound  = errors.New("category not found")
 	ErrCategoryNameEmpty = errors.New("name is required")
 )
+
+var (
+	ErrProductNotFound     = errors.New("product not found")
+	ErrProductNameEmpty    = errors.New("product name is empty")
+	ErrProductPriceInvalid = errors.New("product price must be greater than 0")
+)
---
+++ internal/service/product_service.go
@@ -0,0 +1,71 @@
+package service
+
+import (
+	"basic-go-api/internal/model"
+	"basic-go-api/internal/repository"
+	"context"
+	"database/sql"
+)
+
+type ProductService struct {
+	repo *repository.ProductRepository
+}
+
+func NewProductService(r *repository.ProductRepository) *ProductService {
+	return &ProductService{repo: r}
+}
+
+func (s *ProductService) GetByID(ctx context.Context, id string) (*model.Product, error) {
+	p, err := s.repo.FindByID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	if p == nil {
+		return nil, ErrProductNotFound
+	}
+
+	return p, nil
+}
+
+func (s *ProductService) GetAll(ctx context.Context) ([]model.Product, error) {
+	return s.repo.FindAll(ctx)
+}
+
+func (s *ProductService) Create(ctx context.Context, p *model.Product) error {
+	if p.Name == "" {
+		return ErrProductNameEmpty
+	}
+
+	if p.Price <= 0 {
+		return ErrProductPriceInvalid
+	}
+
+	return s.repo.Create(ctx, p)
+}
+
+func (s *ProductService) Update(ctx context.Context, p *model.Product) error {
+	if p.Name == "" {
+		return ErrProductNameEmpty
+	}
+
+	if p.Price <= 0 {
+		return ErrProductPriceInvalid
+	}
+
+	err := s.repo.Update(ctx, p)
+	if err == sql.ErrNoRows {
+		return ErrProductNotFound
+	}
+
+	return err
+}
+
+func (s *ProductService) Delete(ctx context.Context, id string) error {
+	err := s.repo.Delete(ctx, id)
+	if err == sql.ErrNoRows {
+		return ErrProductNotFound
+	}
+
+	return err
+}
